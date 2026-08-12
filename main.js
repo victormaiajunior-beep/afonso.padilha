61ea56877bdde5fc845f928cedfe5a95e25b18c7
victormaiajunior-beep <victor.maia.junior@escola.pr.gov.br>
2026-08-12 11:07:58 -0300

    l

---
+++ main.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const botao = document.getElementById('meuBotao');
+
+// Adiciona um evento de clique
+botao.addEventListener('click', () => {
+  alert('Você clicou no botão!');
+});
